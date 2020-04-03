8c7f5445e60041875fbf1d88e6f482539d6670f3
Steven Perfors <stevecanbuild@gmail.com>
2020-04-03 22:19:11 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,8 +6,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       h.string :theme 
       h.float :price 
       h.boolean :family_friendly
-      h.integer :opening_date 
-      h.integer :closing_date 
+      h.datetime :opening_date 
+      h.datetime :closing_date 
       h.text :description 
     end
   end
